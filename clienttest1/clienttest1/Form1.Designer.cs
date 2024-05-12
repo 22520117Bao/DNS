eecac1cb670c41f8e5e90687c12780d7652a9f92
22520117Bao <162546070+22520117Bao@users.noreply.github.com>
2024-05-12 18:58:52 +0700

    Add files via upload

---
+++ clienttest1/clienttest1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace clienttest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnResolve = new System.Windows.Forms.Button();
+            this.txtServerIP = new System.Windows.Forms.TextBox();
+            this.df = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDomain = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(590, 62);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(142, 20);
+            this.txtPort.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "client";
+            // 
+            // btnResolve
+            // 
+            this.btnResolve.Location = new System.Drawing.Point(76, 159);
+            this.btnResolve.Name = "btnResolve";
+            this.btnResolve.Size = new System.Drawing.Size(75, 23);
+            this.btnResolve.TabIndex = 2;
+            this.btnResolve.Text = "accept";
+            this.btnResolve.UseVisualStyleBackColor = true;
+            this.btnResolve.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtServerIP
+            // 
+            this.txtServerIP.Location = new System.Drawing.Point(202, 62);
+            this.txtServerIP.Name = "txtServerIP";
+            this.txtServerIP.Size = new System.Drawing.Size(139, 20);
+            this.txtServerIP.TabIndex = 3;
+            this.txtServerIP.TextChanged += new System.EventHandler(this.txtServerIP_TextChanged);
+            // 
+            // df
+            // 
+            this.df.AutoSize = true;
+            this.df.Location = new System.Drawing.Point(436, 68);
+            this.df.Name = "df";
+            this.df.Size = new System.Drawing.Size(26, 13);
+            this.df.TabIndex = 4;
+            this.df.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Domain";
+            // 
+            // txtDomain
+            // 
+            this.txtDomain.Location = new System.Drawing.Point(202, 117);
+            this.txtDomain.Name = "txtDomain";
+            this.txtDomain.Size = new System.Drawing.Size(100, 20);
+            this.txtDomain.TabIndex = 6;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(202, 161);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(437, 176);
+            this.txtResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtDomain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.df);
+            this.Controls.Add(this.txtServerIP);
+            this.Controls.Add(this.btnResolve);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnResolve;
+        private System.Windows.Forms.TextBox txtServerIP;
+        private System.Windows.Forms.Label df;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDomain;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ clienttest1/clienttest1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+namespace clienttest1
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string serverIP = txtServerIP.Text;
+            int serverPort = int.Parse(txtPort.Text);
+            string domain = txtDomain.Text;
+
+            try
+            {
+                using (TcpClient client = new TcpClient(serverIP, serverPort))
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] buffer = Encoding.ASCII.GetBytes(domain);
+                    stream.Write(buffer, 0, buffer.Length);
+
+                    buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    txtResult.Text = response;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtServerIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
